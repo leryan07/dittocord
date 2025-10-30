8d294983cd3da22de7e8abe1f613fc44a2d892f8
Ryan <leryan07@gmail.com>
2025-10-30 11:28:07 -0500

    Add the initial screens (home, notifications, you) with common styling

---
+++ components/BaseText.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import { StyleSheet, Text, TextStyle } from "react-native";
+
+type BaseTextProps = {
+    children: ReactNode;
+    style?: TextStyle;
+};
+
+export default function BaseText({ children, style }: BaseTextProps) {
+    return (
+        <Text style={[styles.base, style]}>
+            {children}
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    base: {
+        color: '#FFF',
+    },
+});
